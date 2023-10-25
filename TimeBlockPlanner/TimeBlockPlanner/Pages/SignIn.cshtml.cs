51d52d8778086833b2ebbd938e8924d868860716
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2023-10-25 12:54:51 -0500

    Added SignIn and SignUp pages to have users login and create an account.

---
+++ TimeBlockPlanner/TimeBlockPlanner/Pages/SignIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeBlockPlanner.Pages
+{
+    public class SignInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
