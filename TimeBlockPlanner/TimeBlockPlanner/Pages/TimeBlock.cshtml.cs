0ecdf50c507e17753414753e415aa24aa5b0e876
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2023-10-25 12:27:53 -0500

    Created some default Razor pages for our application

---
+++ TimeBlockPlanner/TimeBlockPlanner/Pages/TimeBlock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TimeBlockPlanner.Pages
+{
+    public class TimeBlockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
