b7e3c4f58e1cbb3e819a4dd63375aac630fe8b53
aubrianagonzalez <Aubrianagonzalez@ksu.edu>
2023-11-16 17:45:57 -0600

    Added Goals and Users

---
+++ TimeBlockPlanner/TimeBlockData/Models/TimeBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBlockData.Models
+{
+    public class TimeBlock
+    {
+        public int GoalsID { get; }
+        public int UserID { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public DateTime Date { get; }
+        
+    }
+}
--- TimeBlockPlanner/TimeBlockData/Models/User.cs
+++ TimeBlockPlanner/TimeBlockData/Models/User.cs
@@ -8,5 +8,29 @@ namespace TimeBlockData.Models
 {
     public class User
     {
+        public int UserID { get; }
+
+        public string Username { get; }
+
+        public string Email { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string PasswordHash { get; }
+
+        public bool IsDeleted = false;
+
+        public User(int userID, string un, string email, string fn, string ln, string passhash, bool isDeleted)
+        {
+            UserID = userID;
+            Username = un;
+            Email = email;
+            FirstName = fn;
+            LastName = ln;
+            PasswordHash = passhash;
+            IsDeleted = isDeleted;
+        }
     }
 }
