2eaa2cfa08b7b2464b1022676bc08478f0d20461
reagan00010100 <rphazell@ksu.edu>
2023-11-16 16:24:04 -0600

    adding SqlUserRepo utilizing the IUserRepository interface

---
+++ TimeBlockPlanner/UserData/SQL/SqlUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Transactions;
+using UserData.Models;          // needs to be instantiated in this project to resolve the error
+
+namespace UserData.SQL
+{
+    public class SqlUserRepository : IUserRepository
+    {
+
+    }
+}
