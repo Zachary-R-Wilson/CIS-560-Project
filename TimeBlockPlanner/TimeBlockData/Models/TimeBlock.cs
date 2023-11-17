4961d77f67f7c7c4e4320a2a8db64139cad42b45
aubrianagonzalez <Aubrianagonzalez@ksu.edu>
2023-11-16 18:05:32 -0600

    TImeBlock done

--- TimeBlockPlanner/TimeBlockData/Models/TimeBlock.cs
+++ TimeBlockPlanner/TimeBlockData/Models/TimeBlock.cs
@@ -8,11 +8,33 @@ namespace TimeBlockData.Models
 {
     public class TimeBlock
     {
-        public int GoalsID { get; }
+        public int TimeBlockID { get; }
         public int UserID { get; }
         public string Name { get; }
         public string Description { get; }
         public DateTime Date { get; }
-        
+        private string _timePeriod = "";
+        public string TimePeriod
+        {
+            get
+            {
+                // Get the current time in 24-hour format (HH:mm)
+                string time = DateTime.Now.ToString("HH:mm");
+                return time;
+            }
+            set
+            {
+                _timePeriod = value;
+            }
+        }
+        public TimeBlock(int timeBlockId, int userID, string name, string description, DateTime date, string timeP)
+        {
+            TimeBlockID = timeBlockId;
+            UserID = userID;
+            Name = name;
+            Description = description;
+            Date = date;
+            TimePeriod = timeP;
+        }
     }
 }
