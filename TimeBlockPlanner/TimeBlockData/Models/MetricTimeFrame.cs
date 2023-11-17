79da88e126677d90d7f6dfb83316f31b2879faf8
aubrianagonzalez <Aubrianagonzalez@ksu.edu>
2023-11-16 18:27:01 -0600

    Models done

---
+++ TimeBlockPlanner/TimeBlockData/Models/MetricTimeFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBlockData.Models
+{
+    public class MetricTimeFrame 
+    {
+        public int MetricTimeFrameID { get; }
+        public string Name { get; }
+        public bool IsDeleted = false;
+
+        public MetricTimeFrame(int metric, string name, bool isDeleted)
+        {
+            MetricTimeFrameID = metric;
+            Name = name;
+            IsDeleted = isDeleted;
+        }
+    }
+}
---
+++ TimeBlockPlanner/TimeBlockData/Models/UserMetric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBlockData.Models
+{
+    public class UserMetric 
+    {
+        public int UserID { get; }
+        public MetricTimeFrame metricTimeFrame
+        {
+            get;
+        }
+        public Metric metric { get; }
+        public DateTime Date { get; }
+        public UserMetric(int userID, MetricTimeFrame metricTF, Metric _metric, DateTime date)
+        {
+            UserID = userID;
+            metricTimeFrame = metricTF;
+            metric = _metric;
+            Date = date;
+        }
+    }
+}
+
