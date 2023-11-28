49ff252151ff3078d7422d8a043a521cabef088b
reagan00010100 <rphazell@ksu.edu>
2023-11-27 22:42:31 -0600

    added tablels sql for the rest of the model, need to complete the C# and sql procedures for Metric, metrictimeframe, and usermetric

---
+++ TimeBlockPlanner/UserData/SQL/Tables/User.MetricTimeframe.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'User.MetricTimeframe') IS NULL
+BEGIN
+   CREATE TABLE [User].MetricTimeframe
+   (
+      MetricTimeframeId INT NOT NULL,
+      [Name] NVARCHAR(32) NOT NULL,
+      IsDeleted INT NOT NULL
+
+      CONSTRAINT [PK_User_MetricTimeframe_MetricTimeframeId] PRIMARY KEY CLUSTERED
+      (
+         MetricTimeframeId ASC
+      ),
+
+   );
+END
+
+/****************************
+ * Unique Constraints
+ ****************************/
+
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.key_constraints kc
+      WHERE kc.parent_object_id = OBJECT_ID(N'[User].MetricTimeframe')
+         AND kc.[name] = N'UK_User_MetricTimeframe_MetricTimeframeId_Name'
+   )
+BEGIN
+   ALTER TABLE [User].MetricTimeframe
+   ADD CONSTRAINT [UK_User_MetricTimeframe_MetricTimeframeId_Name] UNIQUE NONCLUSTERED
+   (
+      MetricTimeframeId,
+      [Name]
+   )
+END;
---
+++ TimeBlockPlanner/UserData/SQL/Tables/User.UserMetric.sql
@@ -0,0 +1,106 @@
+﻿IF OBJECT_ID(N'User.UserMetric') IS NULL
+BEGIN
+   CREATE TABLE [User].UserMetric
+   (
+      UserId INT NOT NULL,
+      MetricTimeframeId INT NOT NULL,
+      MetricId INT NOT NULL,
+      [Date] DATETIME NOT NULL,
+      [Value] INT NOT NULL
+
+      CONSTRAINT FK_User_User_UserId FOREIGN KEY(UserId)
+      REFERENCES [User].[User](UserId),
+      
+      CONSTRAINT FK_User_MetricTimeframe_MetricTimeframeId FOREIGN KEY(MetricTimeframeId)
+      REFERENCES [User].[MetricTimeframe](MetricTimeframeId),
+
+      CONSTRAINT FK_User_Metric_MetricId FOREIGN KEY(MetricId)
+      REFERENCES [User].[Metric](MetricId),
+   );
+END
+
+/****************************
+ * Unique Constraints
+ ****************************/
+
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.key_constraints kc
+      WHERE kc.parent_object_id = OBJECT_ID(N'[User].UserMetric')
+         AND kc.[name] = N'UK_User_UserMetric_UserId_MetricTimeframeId_MetricId'
+   )
+BEGIN
+   ALTER TABLE [User].UserMetric
+   ADD CONSTRAINT [UK_User_UserMetric_UserId_MetricTimeframeId_MetricId_Date] UNIQUE NONCLUSTERED
+   (
+      UserId,
+      MetricTimeframeId,
+      MetricId,
+      [Date]
+   )
+END;
+
+/****************************
+ * Foreign Keys Constraints
+ ****************************/
+
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.foreign_keys fk
+      WHERE fk.parent_object_id = OBJECT_ID(N'[User].UserMetric')
+         AND fk.referenced_object_id = OBJECT_ID(N'[User].User')
+         AND fk.[name] = N'FK_User_User_UserId'
+   )
+BEGIN
+   ALTER TABLE [User].UserMetric              
+   ADD CONSTRAINT [FK_User_User_UserId] FOREIGN KEY
+   (
+      UserId
+   )
+   REFERENCES [User].[User]
+   (
+      UserId
+   );
+END;
+
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.foreign_keys fk
+      WHERE fk.parent_object_id = OBJECT_ID(N'[User].UserMetric')
+         AND fk.referenced_object_id = OBJECT_ID(N'[User].MetricTimeframe')
+         AND fk.[name] = N'FK_User_MetricTimeframe_MetricTimeframeId'
+   )
+BEGIN
+   ALTER TABLE [User].UserMetric              
+   ADD CONSTRAINT [FK_User_MetricTimeframe_MetricTimeframeId] FOREIGN KEY
+   (
+      MetricTimeframeId
+   )
+   REFERENCES [User].[MetricTimeframe]
+   (
+      MetricTimeframeId
+   );
+END;
+
+IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.foreign_keys fk
+      WHERE fk.parent_object_id = OBJECT_ID(N'[User].UserMetric')
+         AND fk.referenced_object_id = OBJECT_ID(N'[User].Metric')
+         AND fk.[name] = N'FK_User_Metric_MetricId'
+   )
+BEGIN
+   ALTER TABLE [User].UserMetric              
+   ADD CONSTRAINT [FK_User_Metric_MetricId] FOREIGN KEY
+   (
+      MetricId
+   )
+   REFERENCES [User].[Metric]
+   (
+      MetricId
+   );
+END;
