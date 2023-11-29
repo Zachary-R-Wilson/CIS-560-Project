70596ee33e1f5dfcb6364c6fd57080844570410a
reagan00010100 <rphazell@ksu.edu>
2023-11-29 16:40:01 -0600

    added GetMetricTimeframeIdGivenNameDelegate in order to get id from name... need to add other classes

---
+++ TimeBlockPlanner/UserData/DataDelegates/GetMetricTimeframeIdGivenNameDelegate.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserData.Models;
+
+namespace UserData.DataDelegates
+{
+    public class GetMetricTimeframeIdGivenNameDelegate : DataReaderDelegate<MetricTimeframe>
+    {
+        private readonly string name;
+
+        public GetMetricTimeframeIdGivenNameDelegate(string name)
+           : base("User.GetMetricTimeframeIdGivenName")
+        {
+            this.name = name;
+        }
+
+        public override void PrepareCommand(Command command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("Name", name);
+        }
+
+        public override MetricTimeframe Translate(Command command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                return null;
+
+            return new MetricTimeframe(
+               reader.GetInt32("MetricTimeframeId"),
+               name,
+               reader.GetInt32("IsDeleted"));
+        }
+    }
+}
+
