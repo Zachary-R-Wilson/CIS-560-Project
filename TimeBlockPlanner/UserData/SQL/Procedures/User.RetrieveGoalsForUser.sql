5712d80eab486ebbb757b72c63e321a54165501c
Zach <57845873+L0rd-Z-19@users.noreply.github.com>
2023-11-29 11:32:08 -0600

    Added orderbys to timeblock and goal, also implemented a completed checkbox for goals

--- TimeBlockPlanner/UserData/SQL/Procedures/User.RetrieveGoalsForUser.sql
+++ TimeBlockPlanner/UserData/SQL/Procedures/User.RetrieveGoalsForUser.sql
@@ -5,5 +5,6 @@ AS
 SELECT UG.GoalId, UG.UserId, UG.[Name], UG.[Description],
    UG.StartDate, UG.EndDate, UG.IsComplete, UG.Progress
 FROM [User].Goal UG
-WHERE UG.UserId = @UserId;
+WHERE UG.UserId = @UserId AND UG.IsComplete = 1
+ORDER BY UG.EndDate Desc;
 GO
